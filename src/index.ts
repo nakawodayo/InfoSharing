4a791008db2b54e17302b1c5c9fc6917952118ac
nakawodayo <115559628+nakawodayo@users.noreply.github.com>
2022-12-06 20:25:58 +0900

    4 files modified

--- src/index.ts
+++ src/index.ts
@@ -1,30 +1,9 @@
-import { default as config } from './config';
-import { doRequest } from './utils';
-
 const main = async () => {
-  const { qiita_uri } = config;
+  console.log(getArticlesList);
 };
 
-async function getArticlesList(username: string, qiita_uri: string) {
-  let option = {
-    uri: `${qiita_uri}/odata/Users`,
-    method: 'GET',
-    headers: {
-      'Content-Type': 'application/json;charset=utf-8',
-    },
-    json: true,
-  };
-
-  return doRequest(option).then((resArticles: any) => {
-    // console.table(resUsers.value)
-    console.table(
-      resArticles.value.map((article: any) => {
-        return {
-          Title: article.title,
-          Created_At: article.created_at,
-          Likes_Count: article.likes_count,
-        };
-      })
-    );
-  });
+async function getArticlesList() {
+  return (
+    await fetch('https://qiita.com/api/v2/items?query=user%3Anakawodayo')
+  ).json();
 }
